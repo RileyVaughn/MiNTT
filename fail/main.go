94efe2e6e54d57c6d3c20de9faf229aff0c9b4c5
RileyVaughn <rj.vaughn@live.com>
2025-01-20 16:34:36 -0700

    commented out main

--- fail/main.go
+++ fail/main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"github.com/RileyVaughn/MiNTT/numbers"
-	"github.com/RileyVaughn/MiNTT/polynom"
-)
-
 func main() {
 
 	// nums := util.GenRandInt2d(50, 20)
@@ -19,6 +14,6 @@ func main() {
 	// 	[]Intq{Intq{Z: 0, Q: 9}, Intq{Z: 0, Q: 9}, Intq{Z: 0, Q: 9}, Intq{Z: 0, Q: 9}},
 	// }
 
-	var poly polynom.Polynom = polynom.Polynom{numbers.Intq{Z: 0, Q: 9}, numbers.Intq{Z: 5, Q: 9}, numbers.Intq{Z: 7, Q: 9}, numbers.Intq{Z: 6, Q: 9}}
+	//var poly polynom.Polynom = polynom.Polynom{numbers.Intq{Z: 0, Q: 9}, numbers.Intq{Z: 5, Q: 9}, numbers.Intq{Z: 7, Q: 9}, numbers.Intq{Z: 6, Q: 9}}
 
 }
