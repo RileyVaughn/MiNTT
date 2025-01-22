3bdfec144d5b6e7bd6ec37e1fb4770fef4b4f2a3
RileyVaughn <rj.vaughn@live.com>
2025-01-22 16:14:36 -0700

    added consts

---
+++ ineff/const/constants.go
@@ -0,0 +1,6 @@
+package constants
+
+const Q int = 7681
+const N int = 256
+const M int = 16
+const D int = 3
---
+++ ineff/hash/mintt.go
@@ -0,0 +1,6 @@
+package hash
+
+//input will be converted to 0's and 1's
+func MiNTT([]int) {
+
+}
