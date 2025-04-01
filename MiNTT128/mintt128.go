e356c1fe5cb95b0172374f4169d022126fd0df52
RileyVaughn <rj.vaughn@live.com>
2025-04-01 14:19:14 -0600

    m128 ncc

--- MiNTT128/mintt128.go
+++ MiNTT128/mintt128.go
@@ -23,7 +23,7 @@ func MinNTT8(input [ndiv8 * m]byte) [864]byte {
 
 }
 
-func ncc(input [ndiv8]byte) [n]int {
+func ncc(input [ndiv8]byte) [ndiv8][8]int {
 
 	var intermed [ndiv8][8]int
 
@@ -31,6 +31,79 @@ func ncc(input [ndiv8]byte) [n]int {
 		intermed[i] = NTT8_TABLE[input[i]]
 	}
 
+	SIMD_Shift(&intermed[1], 8)
+	SIMD_Shift(&intermed[3], 8)
+	SIMD_Shift(&intermed[5], 8)
+	SIMD_Shift(&intermed[7], 8)
+	SIMD_Shift(&intermed[9], 8)
+	SIMD_Shift(&intermed[11], 8)
+	SIMD_Shift(&intermed[13], 8)
+	SIMD_Shift(&intermed[15], 8)
+
+	SIMD_AddSub(&intermed[0], &intermed[1])
+	SIMD_AddSub(&intermed[2], &intermed[3])
+	SIMD_AddSub(&intermed[4], &intermed[5])
+	SIMD_AddSub(&intermed[6], &intermed[7])
+	SIMD_AddSub(&intermed[8], &intermed[9])
+	SIMD_AddSub(&intermed[10], &intermed[11])
+	SIMD_AddSub(&intermed[12], &intermed[13])
+	SIMD_AddSub(&intermed[14], &intermed[15])
+
+	SIMD_Shift(&intermed[2], 4)
+	SIMD_Shift(&intermed[3], 12)
+	SIMD_Shift(&intermed[6], 4)
+	SIMD_Shift(&intermed[7], 12)
+	SIMD_Shift(&intermed[10], 4)
+	SIMD_Shift(&intermed[11], 12)
+	SIMD_Shift(&intermed[14], 4)
+	SIMD_Shift(&intermed[15], 12)
+
+	SIMD_AddSub(&intermed[0], &intermed[2])
+	SIMD_AddSub(&intermed[1], &intermed[3])
+	SIMD_AddSub(&intermed[4], &intermed[6])
+	SIMD_AddSub(&intermed[5], &intermed[7])
+	SIMD_AddSub(&intermed[8], &intermed[10])
+	SIMD_AddSub(&intermed[9], &intermed[11])
+	SIMD_AddSub(&intermed[12], &intermed[14])
+	SIMD_AddSub(&intermed[13], &intermed[15])
+
+	SIMD_Shift(&intermed[4], 2)
+	SIMD_Shift(&intermed[5], 6)
+	SIMD_Shift(&intermed[6], 10)
+	SIMD_Shift(&intermed[7], 14)
+	SIMD_Shift(&intermed[12], 2)
+	SIMD_Shift(&intermed[13], 6)
+	SIMD_Shift(&intermed[14], 10)
+	SIMD_Shift(&intermed[15], 14)
+
+	SIMD_AddSub(&intermed[0], &intermed[4])
+	SIMD_AddSub(&intermed[1], &intermed[5])
+	SIMD_AddSub(&intermed[2], &intermed[6])
+	SIMD_AddSub(&intermed[3], &intermed[7])
+	SIMD_AddSub(&intermed[8], &intermed[12])
+	SIMD_AddSub(&intermed[9], &intermed[13])
+	SIMD_AddSub(&intermed[10], &intermed[14])
+	SIMD_AddSub(&intermed[11], &intermed[15])
+
+	SIMD_Shift(&intermed[8], 1)
+	SIMD_Shift(&intermed[9], 3)
+	SIMD_Shift(&intermed[10], 5)
+	SIMD_Shift(&intermed[11], 7)
+	SIMD_Shift(&intermed[12], 9)
+	SIMD_Shift(&intermed[13], 11)
+	SIMD_Shift(&intermed[14], 13)
+	SIMD_Shift(&intermed[15], 15)
+
+	SIMD_AddSub(&intermed[0], &intermed[8])
+	SIMD_AddSub(&intermed[1], &intermed[9])
+	SIMD_AddSub(&intermed[2], &intermed[10])
+	SIMD_AddSub(&intermed[3], &intermed[11])
+	SIMD_AddSub(&intermed[4], &intermed[12])
+	SIMD_AddSub(&intermed[5], &intermed[13])
+	SIMD_AddSub(&intermed[6], &intermed[14])
+	SIMD_AddSub(&intermed[7], &intermed[15])
+
+	return intermed
 }
 
 func ntt_sum(input [ndiv8 * m]byte) [N]int {
@@ -50,13 +123,6 @@ func ntt_sum(input [ndiv8 * m]byte) [N]int {
 	}
 
 	return solution
-}
-
-//Takes two int ptrs a and b, replaces *a with *a+*b *b with *a-*b
-func AddSub(a *int, b *int) {
-	temp := *b
-	*b = *a - *b
-	*a = *a + temp
 }
 
 //Assume vaules haveaady been ModQ'd
@@ -79,11 +145,24 @@ func ChangeBase(val [N]int) [864]byte {
 }
 
 //Fake for now
-func SIMD_AddSub() {
-
+func SIMD_AddSub(vec1 *[8]int, vec2 *[8]int) {
+	for i := 0; i < 8; i++ {
+		addSub(&vec1[i], &vec2[i])
+	}
 }
 
 //Fake for now
-func SIMD_Shift() {
+func SIMD_Shift(vec *[8]int, shift int) {
 
+	for i := 0; i < 8; i++ {
+		vec[i] = vec[i] << shift
+	}
+
+}
+
+//Takes two int ptrs a and b, replaces *a with *a+*b *b with *a-*b
+func addSub(a *int, b *int) {
+	temp := *b
+	*b = *a - *b
+	*a = *a + temp
 }
