ef665f3ab4ba8169416bb90e6fc02f1a14d96db9
RileyVaughn <rj.vaughn@live.com>
2025-06-10 15:27:43 -0600

    main setup

--- main.go
+++ main.go
@@ -4,6 +4,19 @@ import (
 	"fmt"
 	"math/rand"
 
+	m128_norm_int64 "github.com/RileyVaughn/MiNTT/hash/int64/normal/MiNTT128"
+	m64_norm_int64 "github.com/RileyVaughn/MiNTT/hash/int64/normal/MiNTT64"
+
+	m128_simd_int64 "github.com/RileyVaughn/MiNTT/hash/int64/simd/MiNTT128"
+	m64_simd_int64 "github.com/RileyVaughn/MiNTT/hash/int64/simd/MiNTT64"
+
+	m128_simd_int16 "github.com/RileyVaughn/MiNTT/hash/int16/simd/MiNTT128"
+	m64_simd_int16 "github.com/RileyVaughn/MiNTT/hash/int16/simd/MiNTT64"
+	m8_simd_int16 "github.com/RileyVaughn/MiNTT/hash/int16/simd/MiNTT8"
+
+	m128_norm_int16 "github.com/RileyVaughn/MiNTT/hash/int16/normal/MiNTT128"
+	m64_norm_int16 "github.com/RileyVaughn/MiNTT/hash/int16/normal/MiNTT64"
+	m8_norm_int16 "github.com/RileyVaughn/MiNTT/hash/int16/normal/MiNTT8"
 	"github.com/RileyVaughn/MiNTT/hash/int64/util"
 )
 
@@ -31,5 +44,24 @@ func main() {
 		}
 
 	}
+
+}
+
+// Sets up all hash variants
+func Setup() {
+
+	m8_norm_int16.SetupM8()
+	m64_norm_int16.SetupM64()
+	m128_norm_int16.SetupM128()
+
+	m8_simd_int16.SetupM8()
+	m64_simd_int16.SetupM64()
+	m128_simd_int16.SetupM128()
+
+	m128_norm_int64.SetupM128()
+	m64_norm_int64.SetupM64()
+
+	m128_simd_int64.SetupM128()
+	m64_simd_int64.SetupM64()
 
 }
